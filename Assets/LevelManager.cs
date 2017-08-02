41bc3120bbcd339cd928f441ff16af1c5d2fbdbf
Hansel Perez <hanselperez1@gmail.com>
2017-08-02 07:34:53 -0400

    Added functions to buttons.

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log ("Level load requested for: " + name);
+	}
+
+	public void QuitLevel(string name) {
+		Debug.Log ("Quit level.");
+	}
+
+}
+
